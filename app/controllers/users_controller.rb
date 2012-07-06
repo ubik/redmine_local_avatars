ac461c5632733356ef0d5ed1093da582a2bd8c7a
Steffen Schüssler <software@neffets.de>
2012-07-06 22:48:40 +0200

    patched version of the last 3 years, for compatibility redmine 1.0 till redmine 1.4-stable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,27 @@ require 'redmine'
 require_dependency 'users_controller' 
 
 class UsersController < ApplicationController
-  helper :attachments
   include AttachmentsHelper 
+
+  helper :attachments
   def save_avatar
-		@user = User.find(params[:id])
+    @user = User.find(params[:id])
     file_field = params[:avatar] rescue nil
-		@user.attachments.clear
-		attachments = attach_files(@user, {'first' => {'file' => file_field, 'description' => 'avatar'}}) 
-		flash[:notice] = l :message_avatar_uploaded
-		@user.save
-		redirect_to :action => 'edit', :id => @user
+    @user.attachments.clear
+    attachments = Attachment.attach_files(@user, {'first' => {'file' => file_field, 'description' => 'avatar'}}) 
+    flash[:notice] = l :message_avatar_uploaded
+    @user.save
+    redirect_to :action => 'edit', :id => @user
   end
   def get_avatar
-		@user = User.find(params[:id])
+    @user = User.find(params[:id])
     av = @user.attachments.find_by_description 'avatar'
-    send_file(av.diskfile, :filename => filename_for_content_disposition(av.filename),
-                                    :type => av.content_type, 
-                                    :disposition => (av.image? ? 'inline' : 'attachment')) if av 
+    if av.nil?
+      super
+    else
+      send_file(av.diskfile, :filename => filename_for_content_disposition(av.filename),
+                             :type => av.content_type, 
+                             :disposition => (av.image? ? 'inline' : 'attachment')) if av 
+    end
   end
 end
--- lib/users_avatar_patch.rb
+++ lib/users_avatar_patch.rb
@@ -1,16 +1,27 @@
-require 'application'
-#require 'user' 
+require 'application_controller'
+require_dependency 'principal' 
+require_dependency 'user' 
 
 module LocalAvatarsPlugin
   module UsersAvatarPatch
     def self.included(base) # :nodoc:    
       base.class_eval do      
         unloadable
-  			has_many :attachments, {:as => :container,
-  															:order => "#{Attachment.table_name}.created_on",
-  															:dependent => :destroy}
+        acts_as_attachable
+        has_many :attachments, { :as => :container,
+                                 :order => "#{Attachment.table_name}.created_on",
+                                 :dependent => :destroy}
+        # deleting attachments requires the attachment to belong to a project
+        belongs_to :project
+
+        def attachments_deletable?(usr)
+          usr == User.current || User.current.admin?   
+        end
+
       end
     end
   end
 end
+
 User.send(:include, LocalAvatarsPlugin::UsersAvatarPatch)
+
